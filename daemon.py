929481503b4c21361fc551567a3ac6a2f3511788
Lucas <lucky-olga@web.de>
2019-11-16 23:32:59 +0100

    add heartbeat for sensor

--- daemon.py
+++ daemon.py
@@ -1,14 +1,50 @@
-import logging
 import reader as r
+import threading
 from gpiozero import LED, Button
 from readings_repo import input_scan_data
+from subprocess import call
+from time import sleep
+from reader import ip
+
+button = Button(4)
+ledwifi = LED(16)
+ledscanner = LED(20)
+ledpower = LED(21)
+
+
+def ping(host):
+    return call(["ping", "-c", "4", "-w", "2", host]) == 0
+
+
+def sensor_heartbeat():
+    while True:
+        sleep(15)
+        if ping(ip):
+            ledwifi.on()
+        else:
+            ledwifi.off()
 
 
 def event_loop():
-    button = Button(4)
+    # setup the LEDs to be active at start
+    ledwifi.on()
+    ledscanner.on()
+    ledpower.on()
+
+    # start the sensor_heartbeat
+    t = threading.Thread(target=sensor_heartbeat)
+    t.start()
+
     while True:
         button.wait_for_press()
-        input_scan_data(r.retrieve_serials(10, 1))
+
+        data = r.retrieve_serials(10, 1)
+        if data is None:
+            # indicate connection failure
+            ledscanner.off()
+        # if data is not empty, push it to the database
+        elif data:
+            input_scan_data(data)
 
 
 event_loop()
