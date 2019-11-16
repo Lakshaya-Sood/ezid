718dd400a60ee8d20b4149052f682752e49e9291
Lucas <lucky-olga@web.de>
2019-11-16 02:52:55 +0100

    implement serial parsing

--- reader.py
+++ reader.py
@@ -1,12 +1,22 @@
 import requests as r
 
+
 def start_scan():
     r.post("http://169.254.57.201/api/MIStartInventory")
+
 
 def stop_scan():
     r.post("http://169.254.57.201/api/MIStopInventory")
 
+    
+def parse_serial(entry):
+    # assuming that last 7 bytes of EPC are the serial
+    entry["serial"] = int("".join(map(lambda s: hex(s)[2:],
+                                      entry["EPC"][-7:])), 16)
+    return entry
+    
+
 def retrieve_serials():
     data = r.get("http://169.254.57.201/api/QuickstartInventoryVar").json()
-
-    return data
+    
+    return map(parse_serial, data["data"]["QuickstartInventoryVar"]["Tags"])
