05344be8bf07b2b62ac788f64bbe3581da2a0870
Lucas <lucky-olga@web.de>
2019-11-16 16:57:57 +0100

    remove unused data push function

--- daemon.py
+++ daemon.py
@@ -1,22 +1,14 @@
 import logging
 import reader as r
 from gpiozero import LED, Button
-from pymongo import MongoClient
-
-
-def push_data(data):
-    # client = MongoClient(
-    #     "mongodb+srv://ezid_usr:8ObxwonM7WBclGOr@ezid-8j9dg.gcp.mongodb.net/test?retryWrites=true&w=majority"
-    # )
-    # db = client.test
-    # collection = db.test
-    # collection.insert_one(data)
-    print(data)
-    print("\n\n\n\n")
+from readings_repo import input_scan_data
 
 
 def event_loop():
     button = Button(4)
     while True:
         button.wait_for_press()
-        push_data(r.retrieve_serials(10))
+        input_scan_data(r.retrieve_serials(10))
+
+
+event_loop()
