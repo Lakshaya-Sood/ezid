b8c1f4af909bd75245222247ec522ffd8cbada71
Lucas <lucky-olga@web.de>
2019-11-16 17:06:24 +0100

    pep8 compliant spacing

--- mongo_connector.py
+++ mongo_connector.py
@@ -4,8 +4,8 @@ import ssl
 ATLAS_SECRET = open("./mongodb_secret.txt").readlines()[0].strip()
 ATLAS_USER = "ezid_usr"
 
-CONNECTION_URL ="mongodb+srv://"+ATLAS_USER+":"+ATLAS_SECRET+"@ezid-8j9dg.gcp.mongodb.net/test?retryWrites=true&w=majority"
-DB_NAME="ezid"
+CONNECTION_URL = "mongodb+srv://" + ATLAS_USER + ":" + ATLAS_SECRET + "@ezid-8j9dg.gcp.mongodb.net/test?retryWrites=true&w=majority"
+DB_NAME = "ezid"
 
 client = MongoClient(CONNECTION_URL, ssl_cert_reqs=ssl.CERT_NONE)
 db = client[DB_NAME]
