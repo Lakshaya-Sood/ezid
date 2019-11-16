bd021e0bfca629cabb6042da7a4c419ac08dcf25
Lucas <lucky-olga@web.de>
2019-11-16 16:47:18 +0100

    data transformation improvements

--- reader.py
+++ reader.py
@@ -1,14 +1,43 @@
 import requests as r
 import socket
+from time import time, sleep
+import threading
 
 base_url = "http://169.254.57.201/api/"
 ip = "169.254.57.201"
+uuid = open("./uuid.txt").readlines()[0].strip()
+
+example_data = [b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD67 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD67 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD61 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD61 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA7 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA7 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA9 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD5F 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA9 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD5F 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA5 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA5 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6A 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6D 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD6D 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD64 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA8 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD64 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA9 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD67 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA9 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD67 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA6 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD61 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA5 1 FDA6 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD61 0 0 0 96 0 0 0\x03',
+                b'\x02sAN IVSingleInv 1 2 1C 34003314F7D5040000003B9ADEA2 1 FDA6 0 0 0 96 0 0 0 1C 34003314F7D5040000003B9ADEA6 1 FD61 0 0 0 96 0 0 0\x03']
 
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 
 def start_scan():
@@ -19,7 +48,7 @@ def stop_scan():
     r.post(base_url + "MIStopInventory")
 
 
-def parse_chunk(chunk):    
+def parse_chunk(chunk):
     return {"uii": chunk[1],
             "antenna_id": chunk[2],
             "rssi": chunk[3:7],
@@ -34,13 +63,32 @@ def parse_inv(inv):
         return list(data)
 
 
-def retrieve_serials(iterations):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 2111))
-
+def retrieve_serials(duration, threshold):
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.connect((ip, 2111))
     result = []
 
-    for i in range(iterations):
-        s.send(b"\x02sMN IVSingleInv F\x03")
-        data = s.recv(8192)
-        print(parse_inv(data))
+    def retrieve():
+        t = threading.currentThread()
+        while getattr(t, "do_run", True):
+            # s.send(b"\x02sMN IVSingleInv F\x03")
+            # data = s.recv(8192)
+            data = example_data[0]
+            result.append(parse_inv(data))
+            sleep(0.5)  # TODO: remove this when using actual data
+
+    t = threading.Thread(target=retrieve)
+    t.start()
+    sleep(duration)
+    t.do_run = False
+    t.join()
+    # s.close()
+
+    uiis = list(map(lambda inv: inv["uii"].decode("ascii"), flatten(result)))
+    unique = set(filter(lambda uii: uiis.count(uii) > threshold, uiis))
+
+    return list(map(lambda uii:
+                    {"uii": uii,
+                     "timestamp": time(),
+                     "scannerid": uuid},
+                    unique))
