fd10e70c8691e94dc8b08b0fdb3cbe5ee18c69fc
Lucas <lucky-olga@web.de>
2019-11-17 01:14:43 +0100

    fix the database creation

--- db_setup.py
+++ db_setup.py
@@ -1,7 +1,7 @@
 import db_connectors as dc
 cursor = dc.cursor
 
-cursor.execute('''CREATE TABLE readings(
-    uui text,
-    timestamp date,
-    scannerid text)''')
+cursor.execute('''CREATE TABLE readings (
+uui text,
+timestamp date,
+scannerid text);''')
