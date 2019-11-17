5f9e7303b72b8db3ac63e965767bed1eaf5e96f4
Lucas <lucky-olga@web.de>
2019-11-17 01:03:28 +0100

    small formatting and clear_cache implementation

--- readings_repo.py
+++ readings_repo.py
@@ -11,9 +11,11 @@ def input_scan_data(data):
     except errors.ServerSelectionTimeoutError:
         print("offline")
 
+
 def write_to_cache(uui, timestamp, scannerid):
-    return readings_sql.execute("INSERT INTO readings (uui, timestamp, scannerid)VALUES (?,?,?);",
+    return readings_sql.execute("INSERT INTO readings (uui, timestamp, scannerid) VALUES (?,?,?);",
                                 [uui, timestamp, scannerid])
+
 
 def read_from_cache(uui, timestamp, scannerid):
     cur = readings_sql.execute("SELECT * FROM readings WHERE uui=? AND timestamp=? AND scannerid=?;",
@@ -21,3 +23,6 @@ def read_from_cache(uui, timestamp, scannerid):
     return cur.fetchmany()
 
 
+def clear_cache():
+    """Used to clear the cache once the data was successfully uploaded."""
+    readings_sql.execute("DELETE * FROM readings;")
